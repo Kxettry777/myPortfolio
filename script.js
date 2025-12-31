80985e0d3ee50dfb636c0d9fbcbaa7f085906168
Kxettry777 <kxettry777@gmail.com>
2025-12-31 09:54:54 +0545

    adding js file and some correction

---
+++ script.js
@@ -0,0 +1,24 @@
+// Scroll section active link
+
+let sections = document.querySelectorAll("section");
+let navlinks = document.querySelectorAll("header nav a");
+
+window.addEventListener("scroll", () => {
+  let top = window.scrollY;
+
+  sections.forEach((sec) => {
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      navlinks.forEach((link) => {
+        link.classList.remove("active");
+      });
+
+      document
+        .querySelector('header nav a[href*="' + id + '"]')
+        .classList.add("active");
+    }
+  });
+});
